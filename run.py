0dd397f69f98b7538927e92e7ecb8d19d6840eb4
marijavelickovska <marija_velickovska88@hotmail.com>
2025-05-05 17:53:00 +0200

    Add show_welcome function to display welcome message

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import dataQuiz
+
+quiz_questions = dataQuiz.quiz_questions
+print(quiz_questions)
+
+def show_welcome():
+    print("="*40)
+    print("Welcome to the World Capitals Challenge")
+    print("="*40)
+
+
+show_welcome()
+
