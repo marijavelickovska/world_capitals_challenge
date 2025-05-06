291d4b92fa09fe0f1d2ab3ba118eabe293e20317
marijavelickovska <marija_velickovska88@hotmail.com>
2025-05-06 11:14:27 +0200

    Add function to display introduction and how to play section

--- run.py
+++ run.py
@@ -13,5 +13,24 @@ def show_welcome():
     print("="*40)
 
 
-show_welcome()
 
+
+def show_introduction():
+    print("\n--- Introduction ---")
+    print("This is a quiz where you will be asked about the capital cities of various countries.")
+    print("Try to answer as many questions correctly as you can!\n")
+
+
+
+def show_how_to_play():
+    print("\n--- How to Play ---")
+    print("1. You will be asked to name the capital of a given country.")
+    print("2. Four possible capital cities will be offered, labeled A, B, C, and D.")
+    print("3. Type the letter (A, B, C, or D) that corresponds to the correct capital and press Enter.")
+    print("4. You will receive feedback after each question.")
+    print("5. At the end, you'll see your final score.\n")
+
+
+show_welcome()
+show_introduction()
+show_how_to_play()
