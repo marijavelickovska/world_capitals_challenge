4ec5591a4552366b694213a5ad4fffb78877be52
marijavelickovska <marija_velickovska88@hotmail.com>
2025-05-08 14:12:36 +0200

    Update quiz_questions dict to enable displaying the country name when showing the correct answer to the user

--- dataQuiz.py
+++ dataQuiz.py
@@ -1,151 +1,151 @@
 quiz_questions = [
     {
-        "question": "What is the capital of France?",
+        "country": "France",
         "answers": {"A": "Berlin", "B": "Rome", "C": "Paris", "D": "Madrid"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Japan?",
+        "country": "Japan",
         "answers": {"A": "Seoul", "B": "Tokyo", "C": "Beijing", "D": "Hanoi"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Brazil?",
+        "country": "Brazil",
         "answers": {"A": "São Paulo", "B": "Rio de Janeiro", "C": "Brasília", "D": "Salvador"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Canada?",
+        "country": "Canada",
         "answers": {"A": "Toronto", "B": "Montreal", "C": "Vancouver", "D": "Ottawa"},
         "correct_answer": "D"
     },
     {
-        "question": "What is the capital of Russia?",
+        "country": "Russia",
         "answers": {"A": "Saint Petersburg", "B": "Moscow", "C": "Novosibirsk", "D": "Kazan"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Australia?",
+        "country": "Australia",
         "answers": {"A": "Sydney", "B": "Canberra", "C": "Melbourne", "D": "Perth"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Germany?",
+        "country": "Germany",
         "answers": {"A": "Munich", "B": "Frankfurt", "C": "Berlin", "D": "Cologne"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Egypt?",
+        "country": "Egypt",
         "answers": {"A": "Cairo", "B": "Alexandria", "C": "Luxor", "D": "Giza"},
         "correct_answer": "A"
     },
     {
-        "question": "What is the capital of Italy?",
+        "country": "Italy",
         "answers": {"A": "Rome", "B": "Milan", "C": "Venice", "D": "Naples"},
         "correct_answer": "A"
     },
     {
-        "question": "What is the capital of China?",
+        "country": "China",
         "answers": {"A": "Shanghai", "B": "Beijing", "C": "Hong Kong", "D": "Guangzhou"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Spain?",
+        "country": "Spain",
         "answers": {"A": "Barcelona", "B": "Madrid", "C": "Seville", "D": "Valencia"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Mexico?",
+        "country": "Mexico",
         "answers": {"A": "Guadalajara", "B": "Cancun", "C": "Mexico City", "D": "Monterrey"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of India?",
+        "country": "India",
         "answers": {"A": "Mumbai", "B": "New Delhi", "C": "Kolkata", "D": "Bangalore"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of the United Kingdom?",
+        "country": "the United Kingdom",
         "answers": {"A": "Manchester", "B": "London", "C": "Birmingham", "D": "Edinburgh"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Argentina?",
+        "country": "Argentina",
         "answers": {"A": "Córdoba", "B": "Rosario", "C": "Buenos Aires", "D": "Santa Fe"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Turkey?",
+        "country": "Turkey",
         "answers": {"A": "Istanbul", "B": "Ankara", "C": "Izmir", "D": "Antalya"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of South Korea?",
+        "country": "South Korea",
         "answers": {"A": "Busan", "B": "Incheon", "C": "Seoul", "D": "Daegu"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of South Africa?",
+        "country": "South Africa",
         "answers": {"A": "Johannesburg", "B": "Pretoria", "C": "Durban", "D": "Cape Town"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of Sweden?",
+        "country": "Sweden",
         "answers": {"A": "Malmö", "B": "Gothenburg", "C": "Stockholm", "D": "Uppsala"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Norway?",
+        "country": "Norway",
         "answers": {"A": "Bergen", "B": "Stavanger", "C": "Oslo", "D": "Trondheim"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Poland?",
+        "country": "Poland",
         "answers": {"A": "Kraków", "B": "Warsaw", "C": "Wrocław", "D": "Gdańsk"},
         "correct_answer": "B"
     },
     {
-        "question": "What is the capital of the Netherlands?",
+        "country": "the Netherlands",
         "answers": {"A": "Rotterdam", "B": "The Hague", "C": "Amsterdam", "D": "Utrecht"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Saudi Arabia?",
+        "country": "Saudi Arabia",
         "answers": {"A": "Jeddah", "B": "Medina", "C": "Riyadh", "D": "Mecca"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Iran?",
+        "country": "Iran",
         "answers": {"A": "Shiraz", "B": "Isfahan", "C": "Tehran", "D": "Tabriz"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Pakistan?",
+        "country": "Pakistan",
         "answers": {"A": "Karachi", "B": "Lahore", "C": "Islamabad", "D": "Peshawar"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Portugal?",
+        "country": "Portugal",
         "answers": {"A": "Porto", "B": "Coimbra", "C": "Lisbon", "D": "Braga"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Thailand?",
+        "country": "Thailand",
         "answers": {"A": "Chiang Mai", "B": "Phuket", "C": "Bangkok", "D": "Hua Hin"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of Switzerland?",
+        "country": "Switzerland",
         "answers": {"A": "Geneva", "B": "Zurich", "C": "Lausanne", "D": "Bern"},
         "correct_answer": "D"
     },
     {
-        "question": "What is the capital of Austria?",
+        "country": "Austria",
         "answers": {"A": "Graz", "B": "Salzburg", "C": "Vienna", "D": "Linz"},
         "correct_answer": "C"
     },
     {
-        "question": "What is the capital of New Zealand?",
+        "country": "New Zealand",
         "answers": {"A": "Auckland", "B": "Christchurch", "C": "Wellington", "D": "Hamilton"},
         "correct_answer": "C"
     }
